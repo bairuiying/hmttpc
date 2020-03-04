e3bc093fa2a9f78686f826e38da040c1af227c0b
bairuiying <1713138911@qq.com>
2020-03-04 15:57:55 +0800

    登陆模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,18 +6,21 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <!-- 表单 -->
-      <el-form style="margin-top:20px">
+      <!-- 表单 需要绑定model属性 绑定-->
+      <el-form :model="loginForm" :rules="loginRules" style="margin-top:20px">
         <!-- 这是我的表单容器 -->
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+        <!-- prop 表示要绑定的字段名 -->
+        <el-form-item prop="mobile">
+          <!-- 表单域 双向绑定v-model -->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <!-- 表单域 -->
+          <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我同意协议</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked">我同意协议</el-checkbox>
         </el-form-item>
         <el-form-item>
           <el-button style="width:100%" type="primary">登陆</el-button>
@@ -28,21 +31,43 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // 定义一个表单数据
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false
+      },
+      // 定义表单验证规则
+      loginRules: {}
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
 // scoped 只对当前组件有用
 // 如果需要写样式，则要给lang属性赋值
 .login {
-  background-image: url(../../assets/img/login_bg.jpg);
   height: 100vh;
   background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
+  &:before {
+    content: "";
+    background-image: url(../../assets/img/login_bg.jpg);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    filter: blur(5px);
+  }
   .login-card {
-    width: 460px;
+    // background-color: rgba(0, 0, 0, 0);
+    z-index: 2;
+    width: 440px;
     height: 340px;
     .title {
       text-align: center;
