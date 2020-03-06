64d4ba800548009c509bec7dc2575a6824674e60
bairuiying <1713138911@qq.com>
2020-03-06 16:13:46 +0800

    主页模块-左侧导航栏

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="layout-aside">
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt />
+    </div>
+    <!-- 导航菜单 -->
+    <el-menu background-color="#323745" text-color="#adafb5">
+      <!-- 子菜单 没有子菜单的用el-mrnu-item标签 -->
+      <el-menu-item>
+        <i class="el-icon-s-home"></i>
+        <span>首页</span>
+      </el-menu-item>
+
+      <!-- 二级菜单 -->
+      <el-submenu>
+        <!-- 二级菜单  -->
+        <template slot="title">
+          <i class="el-icon-s-grid"></i>
+          <span slot="title">内容管理</span>
+        </template>
+        <el-menu-item>发布文章</el-menu-item>
+        <el-menu-item>内容列表</el-menu-item>
+        <el-menu-item>评论管理</el-menu-item>
+        <el-menu-item>素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu>
+        <!-- 二级菜单  -->
+        <i slot="title" class="el-icon-user"></i>
+        <span slot="title">粉丝管理</span>
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item>
+        <i class="el-icon-user-solid"></i>
+        <span>账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-aside {
+  background-color: #2e2f32;
+  height: 100vh;
+  .title {
+    text-align: center;
+    padding: 10px 0;
+    img {
+      height: 35px;
+    }
+  }
+  .el-menu {
+    border-right: none ;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="home">
-    <h1>我是模板</h1>
-  </div>
+  <!-- el-vontainer是大容器 上下导航栏嵌套在里面 -->
+  <el-container>
+    <!-- 左侧导航栏 -->
+    <el-aside>
+      <layout-aside></layout-aside>
+    </el-aside>
+    <el-container>
+      <!-- 头部导航栏 -->
+      <el-header>
+        <h1>头部</h1>
+      </el-header>
+
+      <!-- 身体区 -->
+      <el-main>
+        <h1>主要区</h1>
+        <!-- 这里防止二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import LayoutAside from '@/components/home/layout-aside'
 // @ is an alias to /src
 
-export default {}
+export default {
+  components: {
+    'layout-aside': LayoutAside
+  }
+}
 </script>
